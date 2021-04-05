0c02b37fb4df52e284fad5fce0c0d51c2f7b6fd6
Aleksandr Kukharenko <sandrk27@gmail.com>
2021-04-05 21:28:04 +0300

    feat: lab2

---
+++ lab2/lab2/lib/mocks/posts.dart
@@ -0,0 +1,20 @@
+List posts = [
+  {
+    "id": 1,
+    "name": "username1",
+    "description": "Hmmm, some description....",
+    "isLiked": true,
+    "commentCount": "100",
+    "likedBy": "Ivan",
+    "dayAgo": "2 days ago"
+  },
+  {
+    "id": 2,
+    "name": "username2",
+    "description": "Good day, nice laba",
+    "isLiked": true,
+    "commentCount": "100",
+    "likedBy": "Alex",
+    "dayAgo": "10 days ago"
+  },
+];
---
+++ lab2/lab2/lib/mocks/stories.dart
@@ -0,0 +1,8 @@
+String name = "username";
+
+List stories = [
+  {"id": 1, "name": "username1"},
+  {"id": 2, "name": "username2"},
+  {"id": 3, "name": "username3"},
+  {"id": 4, "name": "username4"},
+];
